b6c85ae7e2279b0f43167d93ebf5e884eae1fae7
HuzaifaIrfan <huzaifairfan2001@gmail.com>
2025-04-26 20:55:33 +0000

    json insertion test

---
+++ tests/import.test.ts
@@ -0,0 +1,87 @@
+import clientPromise from "../lib/mongodb";
+import { Collection, Db, Document, MongoClient, ObjectId } from 'mongodb';
+
+
+
+import pages from './data/video_feed_crawler.pages.json'; // Local JSON import
+import videos from './data/video_feed_crawler.videos.json'; // Local JSON import
+
+
+
+
+
+
+export interface PageEntry {
+  _id: ObjectId,
+  title: string;
+  url: string;
+  site: number;
+  cat: number;
+  created_at: Date;
+}
+
+export function convertJsonToPageEntry(raw: any): PageEntry {
+  return {
+    _id: new ObjectId(raw._id.$oid),
+    title: raw.title,
+    url: raw.url,
+    site: raw.site,
+    cat: raw.cat,
+    created_at: new Date(raw.created_at.$date),
+  };
+}
+
+
+
+
+export interface VideoEntry {
+  _id: string; // or you can use ObjectId if needed, but seems it's just a string
+  title: string;
+  img: string;
+  site: number;
+  cat: number;
+  created_at: Date;
+}
+
+export function convertJsonToVideoEntry(raw: any): VideoEntry {
+  return {
+    _id: raw._id,
+    title: raw.title,
+    img: raw.img,
+    site: raw.site,
+    cat: raw.cat,
+    created_at: new Date(raw.created_at.$date),
+  };
+}
+
+let client: MongoClient;
+let db: Db;
+let videosCollection: Collection<Document>
+let pagesCollection: Collection<Document>
+
+beforeAll(async () => {
+  client = await clientPromise;
+  db = client.db("video_feed_crawler");
+  videosCollection = db.collection("videos");
+  pagesCollection = db.collection("pages");
+});
+
+afterAll(async () => {
+  await client.close();
+});
+
+describe('MongoDB Import Test', () => {
+  it('should import videos JSON data into MongoDB', async () => {
+    await videosCollection.deleteMany({});
+    const jsonData = videos.map(convertJsonToVideoEntry);
+    const result = await videosCollection.insertMany(jsonData);
+    expect(result.insertedCount).toBe(jsonData.length);
+  });
+
+  it('should import pages JSON data into MongoDB', async () => {
+    await pagesCollection.deleteMany({});
+    const jsonData = pages.map(convertJsonToPageEntry);
+    const result = await pagesCollection.insertMany(jsonData);
+    expect(result.insertedCount).toBe(jsonData.length);
+  });
+});
